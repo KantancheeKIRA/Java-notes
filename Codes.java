37dc44c975e92309e3eaa47a34c63c55888e4b71
KantancheeKIRA <Kantancheekhang@gmail.com>
2023-01-06 16:52:53 -0600

    STuff for coding.

---
+++ Codes.java
@@ -0,0 +1,31 @@
+public class Codes{
+
+    public static void main(String [] args){
+            //making a variable ( it stores information )
+    //datatype name = (name) of datatype;
+    //Java is strictly typed ( needs correct data to be out right)
+    //how to push go to terminal and new one
+    //types of datatypes
+    String aString = "sentences"; //things with multiple letter
+    int aInt = 1; // a number any
+    long aLong = 5000; // a bigger number 
+    double aDouble = 1.60; // a decimal number
+    char achar = 'a'; //a single letter 
+    boolean aBoolean = true; //a true or false command 
+
+    String greeting = "howdy";
+
+    greeting = "how are you";
+
+        //how we print 
+        System.out.println(greeting);
+
+        // opertaions ( do any math )
+        System.out.println(2+2);
+
+        int math = 2+2;
+    
+
+
+    }
+}
